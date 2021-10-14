bf7c13d4ce0875d3e2540ec40dba0f460fb25e59
Okai Yeboah <okai.yeboah@turntabl.io>
2021-10-14 15:21:35 +0000

    override iterator() to use comparator

--- src/main/kotlin/SortedClassroom.kt
+++ src/main/kotlin/SortedClassroom.kt
@@ -1,16 +1,16 @@
 class SortedClassroom(vararg sts: Student) : Classroom<Student>(*sts) {
-    inner class StudentSorted : Comparator<Student> {
+
+    class StudentSorter  : Comparator<Student> {
+
         override fun compare(o1: Student?, o2: Student?): Int = when {
             o1!!.averageGrade() > o2!!.averageGrade() -> 1
             o1.averageGrade() < o2.averageGrade() -> -1
             else -> 0
         }
 
-
-
     }
 
-    fun sortStudents () {
-        students.sortWith(StudentSorted())
+    override fun iterator(): Iterator<Student> {
+       return students.sortedWith(StudentSorter()).iterator()
     }
 }
